6364fd08c1f4c37e385a98a7755c7aca745c549a
felixpanzo150-pr <felixpanzo150@gmail.com>
2025-10-02 19:26:40 +0100

    fixed: Validar os dados de entrada da API com Hibernate Validator.

--- src/main/java/tech/felicxpanzo/picpay/controller/WalletController.java
+++ src/main/java/tech/felicxpanzo/picpay/controller/WalletController.java
@@ -1,5 +1,6 @@
 package tech.felicxpanzo.picpay.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class WalletController {
     }
 
     @PostMapping("/wallets")
-    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDto dto) {
+    public ResponseEntity<Wallet> createWallet(@RequestBody @Valid CreateWalletDto dto) {
 
         var wallet = walletService.createWallet(dto);
 
