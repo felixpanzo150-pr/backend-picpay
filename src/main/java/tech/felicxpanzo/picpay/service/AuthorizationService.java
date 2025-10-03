423df4fd1b4e85f3b71d1e1c4242c53e1d1ebf10
felixpanzo150-pr <felixpanzo150@gmail.com>
2025-10-03 18:10:47 +0100

    fixed: Criar o serviço de autorização externo.

--- src/main/java/tech/felicxpanzo/picpay/service/AuthorizationService.java
+++ src/main/java/tech/felicxpanzo/picpay/service/AuthorizationService.java
@@ -1,1 +1,26 @@
+package tech.felicxpanzo.picpay.service;
 
+import org.springframework.stereotype.Service;
+import tech.felicxpanzo.picpay.client.AuthorizationClient;
+import tech.felicxpanzo.picpay.entity.Transfer;
+import tech.felicxpanzo.picpay.exception.PicPayException;
+
+@Service
+public class AuthorizationService {
+
+    private final AuthorizationClient authorizationClient;
+
+    public AuthorizationService(AuthorizationClient authorizationClient, AuthorizationClient authorizationClient1) {
+
+        this.authorizationClient = authorizationClient1;
+    }
+
+    public boolean isAuthorized(Transfer transfer){
+        var resp = authorizationClient.isAuthorized();
+        if (resp.getStatusCode().isError()) {
+            throw new PicPayException();
+        }
+
+        return resp.getBody().authorized();
+    }
+}
